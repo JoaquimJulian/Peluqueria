058300e5e6dbc80cd9efeb289efaaea52d0ec7eb
Joaquin-Julian <jjuliangalvez03@gmail.com>
2024-11-24 16:58:09 +0100

    estilo vista de la agenda

--- src/application/controllers/AgendaController.java
+++ src/application/controllers/AgendaController.java
@@ -1,30 +1,15 @@
 package application.controllers;
 
 import application.Main;
-import javafx.application.Platform;
-import javafx.fxml.FXML;
 
-import javafx.scene.control.Button;
-
-import javafx.scene.image.ImageView;
 
 public class AgendaController {
-	@FXML
-	private ImageView cerrar;
-	@FXML
-	private ImageView logIn;
-
-    private Main mainApp; // Referencia a Main
+	
+	private Main mainApp; // Referencia a Main
     
- // Este método se llamará desde Main para establecer la referencia
+	// Este método se llamará desde Main para establecer la referencia
     public void setMainApp(Main mainApp) {
         this.mainApp = mainApp;
     }
 
-	public void initialize() {
-		
-		
-    	cerrar.setOnMouseClicked(event -> { Platform.exit(); });
-    	logIn.setOnMouseClicked(event -> mainApp.mostrarVista("LogIn.fxml"));
-	}
 }
